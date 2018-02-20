abc47a58a1ce41cd602cdbf443943303e27ef716
Noorul Bhoelai <noorul@h-o.nl>
2018-02-20 14:03:06 +0100

    G2C-2 Add Integration tests for Guest Quote/Order to Shadow Customer conversion

--- src/Test/integration/ConvertGuestOrderToShadowCustomerTest.php
+++ src/Test/integration/ConvertGuestOrderToShadowCustomerTest.php
@@ -6,9 +6,59 @@
 
 namespace Ho\GuestToShadowCustomer\Test\Integration;
 
-class ConvertGuestOrderToShadowCustomerTest
+use Ho\GuestToShadowCustomer\Api\ConvertGuestOrderToShadowCustomerInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+class ConvertGuestOrderToShadowCustomerTest extends \PHPUnit\Framework\TestCase
 {
-    public function __construct()
+
+    /**
+     * @var \Ho\GuestToShadowCustomer\Api\ConvertGuestOrderToShadowCustomerInterface
+     */
+    protected $_convertGuestOrderToShadowCustomer;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->_objectManager = Bootstrap::getObjectManager();
+        $this->_convertGuestOrderToShadowCustomer = $this->_objectManager->create(ConvertGuestOrderToShadowCustomerInterface::class);
+    }
+
+    /**
+     * @magentoDataFixture Magento/Sales/_files/order.php
+     */
+    public function testConvertGuestOrderToShadowCustomer()
+    {
+        $order = $this->_objectManager->create(OrderInterface::class);
+        $customerRepository = $this->_objectManager->create(CustomerRepositoryInterface::class);
+        $order->loadByIncrementId('100000001');
+        $this->expectException(NoSuchEntityException::class);
+        $customerRepository->get('customer@null.com');
+        $this->_convertGuestOrderToShadowCustomer->execute($order->getId());
+        $customer = $customerRepository->get('customer@null.com');
+        $this->assertEquals('customer@null.com', $customer->getEmail());
+    }
+
+    /**
+     * @magentoDataFixture Magento/Sales/_files/order_with_customer.php
+     * @magentoDataFixture Magento/Sales/_files/order.php
+     */
+    public function testOrderAlreadyAssignedToCustomerException()
+    {
+        $this->testConvertGuestOrderToShadowCustomer();
+        $order = $this->_objectManager->create(OrderInterface::class);
+        $order->loadByIncrementId('100000001');
+        $this->_convertGuestOrderToShadowCustomer->execute($order->getId());
+    }
+
+    public function testOrderAlreadyAssignedToShadowCustomerException()
     {
 
     }
--- src/Test/integration/ConvertGuestQuoteToShadowCustomerTest.php
+++ src/Test/integration/ConvertGuestQuoteToShadowCustomerTest.php
@@ -6,8 +6,16 @@
 
 namespace Ho\GuestToShadowCustomer\Test\Integration;
 
-class ConvertGuestQuoteToShadowCustomerTest
+class ConvertGuestQuoteToShadowCustomerTest extends \PHPUnit\Framework\TestCase
 {
+
+    /**
+     * @magentoDataFixture getQuoteWithProductAndCustomerToOrderDataFixture
+     */
+    public function testConvertGuestQuoteToShadowCustomer()
+    {
+    }
+    
     public static function getQuoteWithProductAndCustomerToOrderDataFixture()
     {
         include __DIR__ . '/_files/quote_with_product_and_customer_to_order.php';
