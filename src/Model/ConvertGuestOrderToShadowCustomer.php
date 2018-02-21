a0193278eaa86e2438e8e0861dad6e39974279b6
Noorul Bhoelai <noorul@h-o.nl>
2018-02-21 15:18:25 +0100

    G2C-2 - Add already assigned exceptions while create order

--- src/Model/ConvertGuestOrderToShadowCustomer.php
+++ src/Model/ConvertGuestOrderToShadowCustomer.php
@@ -6,15 +6,31 @@
 
 namespace Ho\GuestToShadowCustomer\Model;
 
+use Ho\GuestToShadowCustomer\Exception\OrderAlreadyAssignedToCustomerException;
+use Ho\GuestToShadowCustomer\Exception\OrderAlreadyAssignedToShadowCustomerException;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\CustomerRegistry;
 use \Magento\Sales\Api\OrderCustomerManagementInterface;
 
 class ConvertGuestOrderToShadowCustomer implements \Ho\GuestToShadowCustomer\Api\ConvertGuestOrderToShadowCustomerInterface
 {
     protected $_orderCustomerManagement;
 
-    public function __construct(OrderCustomerManagementInterface $orderCustomerManagement)
+    protected $_orderRepository;
+
+    private $_customerRepository;
+
+    protected $_customerRegistry;
+
+    public function __construct(OrderCustomerManagementInterface $orderCustomerManagement,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        CustomerRepositoryInterface $customerRepository,
+CustomerRegistry $customerRegistry)
     {
         $this->_orderCustomerManagement = $orderCustomerManagement;
+        $this->_orderRepository = $orderRepository;
+        $this->_customerRepository = $customerRepository;
+        $this->_customerRegistry = $customerRegistry;
     }
 
 
@@ -23,6 +39,15 @@ class ConvertGuestOrderToShadowCustomer implements \Ho\GuestToShadowCustomer\Api
      */
     public function execute($orderId)
     {
-        $this->_orderCustomerManagement->create($orderId);
+        $order = $this->_orderRepository->get($orderId);
+
+        if ($order->getCustomerId()) {
+            if ($this->_customerRegistry->retrieveSecureData($order->getCustomerId())->getPasswordHash()) {
+                throw new OrderAlreadyAssignedToCustomerException(__("Order already assigned to customer exception"));
+            }
+            throw new OrderAlreadyAssignedToShadowCustomerException(__("Order already assigned to shadow customer exception"));
+        } else {
+            $this->_orderCustomerManagement->create($orderId);
+        }
     }
 }
