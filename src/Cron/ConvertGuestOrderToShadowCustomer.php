d531424d5bd09cd3cd607f3417fdf373d40b2c1e
Noorul Bhoelai <noorul@h-o.nl>
2018-02-22 22:53:04 +0100

    FIN2-137 - Add cron execution for shadow customer

--- src/Cron/ConvertGuestOrderToShadowCustomer.php
+++ src/Cron/ConvertGuestOrderToShadowCustomer.php
@@ -6,7 +6,9 @@
 
 namespace Ho\GuestToShadowCustomer\Cron;
 
+use Ho\GuestToShadowCustomer\Api\ConvertGuestOrderToShadowCustomerInterface;
 use Ho\GuestToShadowCustomer\Api\GuestOrderRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 
 class ConvertGuestOrderToShadowCustomer
 {
@@ -14,18 +16,30 @@ class ConvertGuestOrderToShadowCustomer
     /** @var  GuestOrderRepositoryInterface */
     protected $_guestOrderRepository;
 
+    /** @var SearchCriteriaBuilder */
+    protected $_searchCriteriaBuilder;
 
-    public function __construct(GuestOrderRepositoryInterface $guestOrderRepository)
+
+    public function __construct(GuestOrderRepositoryInterface $guestOrderRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        ConvertGuestOrderToShadowCustomerInterface $convertGuestOrderToShadowCustomer
+    )
     {
-
-        $this->_guestOrderRepository = $this->_objectManager->create(
-            GuestOrderRepositoryInterface::class
-        );
+        $this->_guestOrderRepository = $guestOrderRepository;
+        $this->_searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->_convertGuestOrderToShadowCustomer = $convertGuestOrderToShadowCustomer;
     }
 
 
     public function execute()
     {
+        $searchCriteria = $this->_searchCriteriaBuilder->create();
+        $orders = $this->_guestOrderRepository->getList($searchCriteria);
+        if ($orders->getTotalCount() > 0) {
+            foreach ($orders as $order) {
+                $this->_convertGuestOrderToShadowCustomer->execute($order->getId());
+            }
+        }
 
     }
 }
