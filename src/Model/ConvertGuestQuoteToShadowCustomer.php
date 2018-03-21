f8401c15cc50b69d6d21a71d034560f5672ed52d
Noorul Bhoelai <noorul@h-o.nl>
2018-03-21 01:35:41 +0100

    FIN2-141 - Add is shadow flag on shadow customer save

--- src/Model/ConvertGuestQuoteToShadowCustomer.php
+++ src/Model/ConvertGuestQuoteToShadowCustomer.php
@@ -121,6 +121,7 @@ class ConvertGuestQuoteToShadowCustomer implements ConvertGuestQuoteToShadowCust
             }
             /** @var \Magento\Customer\Api\Data\CustomerInterface $customer */
             $customer = $this->customerFactory->create(['data' => $customerData]);
+            $customer->setCustomAttribute('is_shadow', 1);
             $account = $this->accountManagement->createAccount($customer);
         }
         $quote->setCustomer($account);
