c649f14e5fb2b111bbc20918a924aa658e3aac5a
Noorul Bhoelai <noorul@h-o.nl>
2018-04-02 21:50:52 +0200

    Add quote conversion tests

--- src/Test/integration/ConvertGuestQuoteToShadowCustomerTest.php
+++ src/Test/integration/ConvertGuestQuoteToShadowCustomerTest.php
@@ -8,8 +8,23 @@ declare(strict_types=1);
 
 namespace ReachDigital\GuestToShadowCustomer\Test\Integration;
 
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Sales\Model\OrderRepository;
+use Magento\TestFramework\Helper\Bootstrap;
+
 class ConvertGuestQuoteToShadowCustomerTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $objectManager;
+
+    protected function setup()
+    {
+        parent::setUp();
+        $this->objectManager = Bootstrap::getObjectManager();
+    }
 
     /**
      * @test
@@ -17,6 +32,14 @@ class ConvertGuestQuoteToShadowCustomerTest extends \PHPUnit\Framework\TestCase
      */
     public function should_convert_guest_quote_to_order_with_shadow_customer()
     {
+        /** @var CustomerRepositoryInterface $customerRepository */
+        $customerRepository = $this->objectManager->create(CustomerRepositoryInterface::class);
+        $orderRepository = $this->objectManager->create(OrderRepository::class);
+        $searchCriteriaInterface = $this->objectManager->create(SearchCriteriaInterface::class);
+        $order = $orderRepository->getList($searchCriteriaInterface)->getItems();
+        $customer = $customerRepository->getList($searchCriteriaInterface)->getItems();
+        $this->assertNotEmpty($customer);
+        $this->assertNotEmpty($order);
     }
     
     public static function getQuoteWithProductAndCustomerToOrderDataFixture()
