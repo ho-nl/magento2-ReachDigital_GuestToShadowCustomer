3e500da3d61a438ff57cc726dc34f03b1fdc76ab
Pascal Brouwers <pascal@h-o.nl>
2019-05-08 11:57:37 +0200

    [FIN2-495] Fix showing customer link in order view page

---
+++ src/Plugin/Block/Adminhtml/Order/View/InfoPlugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+namespace ReachDigital\GuestToShadowCustomer\Plugin\Block\Adminhtml\Order\View;
+
+class InfoPlugin
+{
+
+    public function afterGetCustomerViewUrl(\Magento\Sales\Block\Adminhtml\Order\View\Info $subject, $result)
+    {
+        // ShadowCustomer sets the customerId on the order
+        if ($subject->getOrder()->getCustomerId()) {
+            return $subject->getUrl('customer/index/edit', ['id' => $subject->getOrder()->getCustomerId()]);
+        }
+
+        return $result;
+    }
+}
