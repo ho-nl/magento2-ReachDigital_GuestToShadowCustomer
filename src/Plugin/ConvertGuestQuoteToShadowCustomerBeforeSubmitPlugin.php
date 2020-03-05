5b8e3f4b5d2f867993600b5883fb5b6eddfd90e2
Jeroen van Leusden <jeroen@reachdigital.nl>
2020-03-05 11:43:30 +0100

    [SIFU-634] Ability to disable module per website/storeview

--- src/Plugin/ConvertGuestQuoteToShadowCustomerBeforeSubmitPlugin.php
+++ src/Plugin/ConvertGuestQuoteToShadowCustomerBeforeSubmitPlugin.php
@@ -1,20 +1,36 @@
 <?php
 /**
- * Copyright (c) 2018 Reach Digital, http://www.reachdigital.nl
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
  * See LICENSE.txt for license details.
  */
+declare(strict_types=1);
+
 namespace ReachDigital\GuestToShadowCustomer\Plugin;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Model\QuoteManagement;
 use Magento\Quote\Model\Quote;
+use Magento\Store\Model\ScopeInterface;
 use ReachDigital\GuestToShadowCustomer\Api\ConvertGuestQuoteToShadowCustomerInterface;
 
 class ConvertGuestQuoteToShadowCustomerBeforeSubmitPlugin
 {
+    /** @var ScopeConfigInterface $scopeConfig */
+    private $scopeConfig;
+
+    /** @var ConvertGuestQuoteToShadowCustomerInterface $convertGuestQuoteToShadowCustomer */
     private $convertGuestQuoteToShadowCustomer;
 
-    public function __construct(ConvertGuestQuoteToShadowCustomerInterface $convertGuestQuoteToShadowCustomer)
-    {
+    /**
+     * @param ScopeConfigInterface                       $scopeConfig
+     * @param ConvertGuestQuoteToShadowCustomerInterface $convertGuestQuoteToShadowCustomer
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ConvertGuestQuoteToShadowCustomerInterface $convertGuestQuoteToShadowCustomer
+    ) {
+        $this->scopeConfig = $scopeConfig;
         $this->convertGuestQuoteToShadowCustomer = $convertGuestQuoteToShadowCustomer;
     }
 
@@ -23,14 +39,22 @@ class ConvertGuestQuoteToShadowCustomerBeforeSubmitPlugin
      * This is done at this point because the customer ID is needed as 'shopperReference'
      * in the Adyen recurring contract notification
      *
-     * @inheritdoc
+     * @throws NoSuchEntityException
      */
-    public function beforeSubmit(QuoteManagement $subject, Quote $quote, $orderData = [])
+    public function beforeSubmit(QuoteManagement $subject, Quote $quote, $orderData = []): array
     {
-        $this->convertGuestQuoteToShadowCustomer->execute($quote);
+        $enabled = $this->scopeConfig->isSetFlag(
+            'guest_to_shadow_customer/general/enabled',
+            ScopeInterface::SCOPE_STORES,
+            $quote->getStore()
+        );
 
-        $quote->getShippingAddress()->setCustomerId($quote->getCustomer()->getId());
-        $quote->getBillingAddress()->setCustomerId($quote->getCustomer()->getId());
+        if ($enabled === true) {
+            $this->convertGuestQuoteToShadowCustomer->execute($quote);
+
+            $quote->getShippingAddress()->setCustomerId($quote->getCustomer()->getId());
+            $quote->getBillingAddress()->setCustomerId($quote->getCustomer()->getId());
+        }
 
         return [$quote, $orderData];
     }
