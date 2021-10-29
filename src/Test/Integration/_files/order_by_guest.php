c3d95c9f7d1714f6aa9f313b01c3bba9f4d211ff
Maikel Koek <maikel@h-o.nl>
2021-10-29 15:37:25 +0200

    fix: type casting before assertions and corrected file include

--- src/Test/Integration/_files/order_by_guest.php
+++ src/Test/Integration/_files/order_by_guest.php
@@ -3,9 +3,11 @@
  * Copyright © Reach Digital (https://www.reachdigital.io/)
  * See LICENSE.txt for license details.
  */
+
 declare(strict_types=1);
 
-require 'Magento/Sales/_files/guest_quote_with_addresses.php';
+require __DIR__ .
+    '/../../../../../../../dev/tests/integration/testsuite/Magento/Sales/_files/guest_quote_with_addresses.php';
 
 /** @var \Magento\Quote\Model\Quote $quote */
 /** @var \Magento\Framework\ObjectManagerInterface $objectManager */
